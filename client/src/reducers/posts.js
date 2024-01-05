d38e0cf71667c84410fc91aabc052f7a038e1481
Rudy Washington <rawfitgroup@gmail.com>
2024-01-04 22:22:21 -0500

    connecting frontend to backend

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -8,19 +8,19 @@ import {
 
 const postReducer = (posts = [], action) => {
   switch (action.type) {
-    case FETCH_ALL:
+    case 'UPDATE':
+      return posts.map((post) =>
+        post._id === action.payload._id ? action.payload : post
+      );
+    case 'FETCH_ALL':
       return action.payload;
-    case LIKE:
-      return posts.map((post) =>
-        post._id === action.payload._id ? action.payload : post
-      );
-    case CREATE:
+    case 'CREATE':
       return [...posts, action.payload];
-    case UPDATE:
-      return posts.map((post) =>
-        post._id === action.payload._id ? action.payload : post
-      );
-    case DELETE:
+    case 'LIKE':
+        return posts.map((post) =>
+          post._id === action.payload._id ? action.payload : post
+        );  
+    case 'DELETE':
       return posts.filter((post) => post._id !== action.payload);
     default:
       return posts;
